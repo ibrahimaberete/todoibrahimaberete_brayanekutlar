55c89ca0ed8a2a41f69f3e0fd25514441241384f
ibrahimaberete <47101718+ibrahimaberete@users.noreply.github.com>
2022-04-01 10:53:06 +0200

    stage 10

--- app/src/main/java/com/example/todoibrahimaberete_brayanekutlar/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/example/todoibrahimaberete_brayanekutlar/tasklist/TaskListAdapter.kt
@@ -1,30 +1,41 @@
 package com.example.todoibrahimaberete_brayanekutlar.tasklist
 
+import android.system.Os.bind
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todoibrahimaberete_brayanekutlar.R
+
 
 // l'IDE va râler ici car on a pas encore implémenté les méthodes nécessaires
 class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
-    var currentList: List<String>
+    var currentList: List<String> = emptyList()
 
     // on utilise `inner` ici afin d'avoir accès aux propriétés de l'adapter directement
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(taskTitle: String) {
             // on affichera les données ici
+          val textView = itemView.findViewById<TextView>(R.id.task_title)
+          textView.text = taskTitle
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
     }
 
+
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+       holder.bind(currentList[position])
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return currentList.size;
     }
 }
